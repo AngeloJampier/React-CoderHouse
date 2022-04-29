57ff8f0982f2830d908fe4ac976177545509fc9e
AngeloJampier <angelo.20jcp@gmail.com>
2022-04-28 20:43:32 -0500

    GAAA

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Esta es mi primer aplicacion en React.JS</h1>
+      <h2>Mi segunda clase de React</h2>
+      <h3>Mab cagon</h3>
+    </div>
+  );
+}
+
+export default App;
+
